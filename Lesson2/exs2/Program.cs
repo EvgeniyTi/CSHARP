48a78d1e6cf9867114d14e51c2e938d273f1f715
etima <48822855+EvgeniyTi@users.noreply.github.com>
2022-09-26 15:39:48 +0300

    lesson 2 done

---
+++ Lesson2/exs2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите 3х значное число: ");
+string number = Console.ReadLine();
+//Console.WriteLine(typeof(number));
+
+if (number.Length >= 3 && int.Parse(number) > 0)
+{
+    Console.Write("Третья цифра ");
+    Console.WriteLine(number[2]);
+}
+else if (int.Parse(number) < 0)
+{
+    Console.WriteLine("Введите положетельное число более 99 ");
+}
+
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
