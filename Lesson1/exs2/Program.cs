408f96ba4a58354e1c859c195dcb56e4e1dd5133
etima <48822855+EvgeniyTi@users.noreply.github.com>
2022-09-26 15:46:21 +0300

    lesson 2 done_

--- Lesson1/exs2/Program.cs
+++ Lesson1/exs2/Program.cs
@@ -1,6 +1,6 @@
-﻿int num1 = int.Parse(Console.ReadLine());
-int number2 = int.Parse(Console.ReadLine());
-int num3 = int.Parse(Console.ReadLine());
+﻿int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+int num3 = int.Parse(Console.ReadLine()!);
 int max = num1;
 
 if (num2 >= max && num2 >= num3)
