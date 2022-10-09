1a10a58ff6d650e51baf2a81c3a0557701b5ff56
etima <48822855+EvgeniyTi@users.noreply.github.com>
2022-10-09 22:25:38 +0300

    lesson6 done

---
+++ lesson6/exs1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+Console.Clear();
+
+void Main(string args)
+{
+    string[] numbers = args.Split(',');
+
+    int count = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (Convert.ToInt64(numbers[i]) > 0) count++;
+    }
+    //Console.WriteLine(String.Join(",", numbers));
+    Console.Write($"Колличество чисел больше нуля равно --> {count}");
+}
+
+Console.Write("Ввведите с клавиатуры набор чисел: ");
+Main(Console.ReadLine());
